4bc977bdfb56beb1929671f351f382662d7dfd27
Julian <neferin42@gmail.com>
2020-07-02 10:51:05 +0200

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
