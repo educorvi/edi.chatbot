3b4ac944f16e91bf6adfa8b658b445f5761d3682
Julian Pollinger <neferin42@gmail.com>
2020-09-07 14:27:39 +0200

    fixed zip script

--- zip.js
+++ zip.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const fs = require('fs');
 const archiver = require('archiver');
 
@@ -7,21 +8,26 @@ let archiv = archiver('zip', {
     zlib: {level: 9}
 });
 
-archiv.pipe(output);
-archiv.file('./src/chatLogic.js', {name: "chatLogic.js"});
-archiv.directory('./src/lang/', "lang");
-archiv.finalize();
+new Promise((resolve, reject) => {
+    archiv.pipe(output);
+    archiv.file('./src/chatLogic/chatLogic.js', {name: "chatLogic.js"});
+    archiv.directory('./src/chatLogic/lang/', "lang");
+    output.on('close', function () {
+        resolve(true);
+    });
+    archiv.finalize();
+}).then(r => {
+    output = fs.createWriteStream(__dirname + '/demoDist.zip');
+
+    archiv = archiver('zip', {
+        zlib: {level: 9}
+    });
+
+    archiv.pipe(output);
+    archiv.directory('./dist', false);
+    archiv.finalize();
+})
 
 
 
-
-output = fs.createWriteStream(__dirname + '/demoDist.zip');
-
-archiv = archiver('zip', {
-    zlib: {level: 9}
-});
-
-archiv.pipe(output);
-archiv.directory('./dist', false);
-archiv.finalize();
 
